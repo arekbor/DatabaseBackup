19e5965e74055de8d9426e59c007bb25f29108e6
a_r_e_k97 <arekindependent@mgial.com>
2022-07-15 18:38:28 +0200

    init commit

---
+++ DatabaseBackup/JsonReader/DbItems.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+
+namespace DatabaseBackup.JsonReader;
+
+[JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+public class DbItems
+{
+    [JsonProperty("DataSource")]
+    public static string? DataSource { get; set; }
+    [JsonProperty("UserId")]
+    public static string? UserId { get; set; }
+    [JsonProperty("Password")]
+    public static string? Password { get; set; }
+}
---
+++ DatabaseBackup/JsonReader/ReadJson.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace DatabaseBackup.JsonReader;
+
+public static class ReadJson
+{
+    public static async Task InitJson()
+    {
+        using var reader = new StreamReader(@"C:\Users\a_r_e_000\RiderProjects\DatabaseBackup\DatabaseBackup\dbSettings.json");
+        var json = await reader.ReadToEndAsync();
+        var items = JsonConvert.DeserializeObject<DbItems>(json);
+    }
+}
